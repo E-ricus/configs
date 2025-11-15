b987db63d0e02bf122caf317cc3cb8898a6e0db1
Eric Puentes <ericdpb@pm.me>
2025-11-15 10:10:52 +0100

    add virtualization module

---
+++ nix/modules/virtualization.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  pkgs,
+  lib,
+  user,
+  ...
+}: {
+  # Enable QEMU/KVM virtualization
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = false;
+      swtpm.enable = true; # TPM emulation for Windows 11
+      ovmf = {
+        enable = true; # UEFI support
+        packages = [pkgs.OVMFFull.fd];
+      };
+    };
+  };
+
+  # Enable USB redirection for VMs
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  # Install virt-manager and related tools
+  programs.virt-manager.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    virt-viewer # Remote viewer for VMs
+    spice # SPICE protocol for better graphics
+    spice-gtk # SPICE client
+    spice-protocol
+    win-virtio # Windows VirtIO drivers ISO
+    win-spice # Windows SPICE tools
+  ];
+
+  # Add your user to libvirtd group
+  users.users.${user}.extraGroups = ["libvirtd"];
+
+  # Optional: Enable dnsmasq for VM networking
+  virtualisation.libvirtd.onBoot = "ignore"; # Don't auto-start VMs on boot
+  virtualisation.libvirtd.onShutdown = "shutdown"; # Gracefully shutdown VMs
+}
