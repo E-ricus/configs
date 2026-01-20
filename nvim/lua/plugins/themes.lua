c685ee7227bf1f189f96009ec34b1bc9088df480
Eric Puentes <ericdpb@pm.me>
2026-01-20 15:38:29 +0100

    clangd and highlight

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,5 +1,29 @@
 return {
   {
+    -- Generic semantic token customization for all themes
+    "semantic-highlights",
+    dir = vim.fn.stdpath("config"),
+    name = "semantic-highlights",
+    lazy = false,
+    priority = 999, -- Load right after themes (which have priority 1000)
+    config = function()
+      -- Disable grayed out inactive code regions (for all LSPs)
+      local function disable_inactive_regions()
+        vim.api.nvim_set_hl(0, "@lsp.mod.inactive", { link = "Normal" })
+        vim.api.nvim_set_hl(0, "@lsp.mod.unused", { link = "Normal" })
+        -- clang makes the semantic token a comment
+        vim.api.nvim_set_hl(0, "@lsp.type.comment.c", {})
+      end
+
+      disable_inactive_regions()
+
+      -- Set up autocmd to reapply after colorscheme changes
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = disable_inactive_regions,
+      })
+    end,
+  },
+  {
     "catppuccin/nvim",
     enabled = false,
     lazy = false,
